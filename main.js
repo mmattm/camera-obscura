70f8de45388f784f748fac751dbb79431b0bb9dd
Matthieu Minguet <minguet.mat@gmail.com>
2023-04-05 15:45:27 +0200

    batch

--- main.js
+++ main.js
@@ -32,4 +32,11 @@ import { Camera } from "./camerav2";
 // `;
 Camera();
 
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", appHeight);
+appHeight();
+
 // setupCounter(document.querySelector("#counter"));
