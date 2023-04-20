0bae912a5d5d9ed3c0c31ee5200d2dbfb49ec652
Matthieu Minguet <minguet.mat@gmail.com>
2023-04-20 12:13:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,12 @@ import { camera } from "./camera";
 import { appHeight } from "./utils";
 
 import { objectSequence } from "/projects/objectSequence";
-import { emojiStory } from "/projects/emojiStory";
 
 // Lancer la caméra
 camera();
 
 // Configurer les interactions
 objectSequence();
-//emojiStory();
 
 // Fix Ios
 window.addEventListener("resize", appHeight);
