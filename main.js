5cf82d3d43d2278948e24a2acaec3dc0956790e9
Matthieu Minguet <minguet.mat@gmail.com>
2023-04-22 15:01:12 +0200

    project

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import { camera } from "./camera";
 import { appHeight } from "./utils";
 
 import { objectSequence } from "./examples/objectSequence";
+import p5Example from "./examples/p5Example";
 
 // Lancer la caméra
 camera();
 
 // Projet
-objectSequence();
+p5Example();
 
 // Fix Ios
 window.addEventListener("resize", appHeight);
