004ea8138e2966ccff0b78d74e40e6459fd02d79
Matthieu Minguet <minguet.mat@gmail.com>
2023-04-22 15:03:22 +0200

    project

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { camera } from "./camera";
 import { appHeight } from "./utils";
 
 import { objectSequence } from "./examples/objectSequence";
-import p5Example from "./examples/p5Example";
+import { p5Example } from "./examples/p5Example";
 
 // Lancer la caméra
 camera();
